f83c9b9a2f5eba68900d9ed1462b6e8260013990
baadhira <badirakunhabdulla@gmail.com>
2022-06-30 17:15:58 +0530

    bug fixed

--- mypro/mypro/urls.py
+++ mypro/mypro/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.static import serve 
-from django.conf.urls import url
+# from django.views.static import serve 
+# from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/',include('app.urls')),
@@ -12,8 +12,8 @@ urlpatterns = [
     path('discussionapi/',include('discussion.urls')),
     path('chat/',include('chat.urls')),
     path('message/',include('messagecontrol.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
     # path('', include('rest_friendship.urls')),
 ]
